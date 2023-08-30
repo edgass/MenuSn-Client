39e4c7409661ad1e7c41d41bf712b1ea1772fbff
unknown <edaligassama@gmail.com>
2023-08-30 20:11:58 +0200

    Before deploy

--- src/command/command.js
+++ src/command/command.js
@@ -1,15 +1,12 @@
 import { SingleCommandElementInCommand } from "./single_command_element_in_command";
-import {useState,useEffect} from 'react'
+import {useEffect} from 'react'
 import {useAppSelector} from '../hook'
-import {getCommand, setCommand,setShowConfirmCommandModal,setShowDeleteCommandModal } from "../store/command-store";
+import {getCommand, setShowConfirmCommandModal,setShowDeleteCommandModal } from "../store/command-store";
 import {useDispatch} from 'react-redux'
-import Element from "../model/element_model";
-import SingleElementInCommandeModel from "../model/single_element_in_command_model";
 import { ConfirmCommanModal } from "../Plats/confirm_command_modal";
 import { ConfirmDeleteCommandModal } from "../Plats/confirm_delete_command_modal";
 
 export function Command(){
-    const [command, setAppCommand] = useState([]);
     const stateofCommand = useAppSelector(state => state.command);
     const dispatch = useDispatch();
 
@@ -17,8 +14,8 @@ export function Command(){
         dispatch(getCommand())
         console.log(stateofCommand.entities)
         
-      }, []);
-
+      }, [dispatch]);
+/*
       const handleChange = event => {
         // Mettre à jour la valeur de cacheValue lorsque l'utilisateur entre une valeur
         const newValue = event.target.value;
@@ -27,6 +24,7 @@ export function Command(){
         // Enregistrement de la valeur dans localStorage
         localStorage.setItem("command", newValue);
       };
+      */
 
 
     return(
@@ -35,7 +33,7 @@ export function Command(){
        {stateofCommand.showDeleteCommandModal ? <ConfirmDeleteCommandModal/> :null}
         <div className="mx-auto flex flex-col justify-center">
            
-            { stateofCommand.entities.length != 0 ?
+            { stateofCommand.entities.length !== 0 ?
             <div className="w-screen">
             <h1 className="text-left ml-6 text-xl mb-4">Ma Commande</h1>
                 <ul className="flex flex-row gap-0 w-screen overflow-y-auto">
@@ -50,18 +48,18 @@ export function Command(){
                 </ul>
                 
             </div>
-            : <h3 className="w-screen text-center"></h3>}
+            : <h3 className="w-screen text-center">Aucune commande</h3>}
         
         </div>
-        { stateofCommand.entities.length != 0 ?
+        { stateofCommand.entities.length !== 0 ?
         <div className="text-center">
-            <button class="bg-green-500 mx-2 px-3 py-2 my-2 rounded-2xl shadow w-48"
+            <button class="bg-green-400 mx-2 px-3 py-2 my-2 text-white rounded-2xl shadow w-48"
             onClick={
             
              ()=>{dispatch(setShowConfirmCommandModal(true))} 
             }
             >Valider ma commande</button>
-            <button class="bg-red-500 mx-2 px-3 py-2 my-2 rounded-2xl shadow w-48"
+            <button class="bg-orange-500 mx-2 px-3 py-2 my-2 text-white rounded-2xl shadow w-48"
             onClick={
             
              ()=>{
